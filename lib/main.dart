336915de9ac0ad55924991bd074d586043ab7240
Demba <demba94diagana@gmail.com>
2022-01-08 22:32:54 +0000

    Scaffold use

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        brightness: Brightness.light,
-      ),
-      home: Scaffold(appBar: AppBar(title: const Text("App Example"),),),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          brightness: Brightness.light,
+        ),
+        home: const MyHomePage());
   }
 }
 
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Material App Demo Theme"),
+        backgroundColor: Colors.red,
+        // actions: const [Icon(Icons.eighteen_mp_outlined)],
+        elevation: 0.0,
+        centerTitle: true,
+      ),
+      endDrawer: Drawer(
+        child: ListView(
+          children: const <Widget>[
+            DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'Menu',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ))
+          ],
+        ),
+      ),
+      bottomNavigationBar:
+          BottomNavigationBar(items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Business'),
+        BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
+      ]),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {}, label: const Text('Approve'),
+          icon: const Icon(Icons.thumb_up),
+          backgroundColor: Colors.pink,),
+      body: const Center(
+        child: Text(
+          "Hello World",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
